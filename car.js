3facdf728a86a3654224c2ef7bcd17f7b2121ebd
Rod Vagg <rod@vagg.org>
2020-09-14 15:32:16 +1000

    complete CarWriter functionality, w/ backpressure

--- car.js
+++ car.js
@@ -27,14 +27,48 @@ function CarWriter (Block) {
   const { CID } = Block.multiformats
   const queue = []
   let resolver = null
+  let drainer = null
+  let drainerResolver = null
   let ended = false
+  let iterating = false
+
+  function toRoots (roots) {
+    if (roots && roots.asCID === roots) {
+      roots = [roots]
+    }
+    if (roots === undefined) {
+      roots = []
+    }
+    if (!Array.isArray(roots)) {
+      throw new TypeError('roots must be a single CID or an array of CIDs')
+    }
+    const _roots = []
+    for (const root of roots) {
+      const _root = CID.asCID(root)
+      if (!_root) {
+        throw new TypeError('roots must be a single CID or an array of CIDs')
+      }
+      _roots.push(_root)
+    }
+    return _roots
+  }
 
   const iterator = {
     async next () {
       if (queue.length) {
         return { done: false, value: queue.shift() }
+      } else {
+        if (drainer) {
+          drainerResolver()
+          drainerResolver = null
+          drainer = null
+        }
       }
 
+      // This is a special case that shouldn't be possible with the mutex and
+      // drainer queue. Writes are blocked in deferred Promises until data is
+      // consumed.
+      /* c8 ignore next 3 */
       if (ended) {
         return { done: true }
       }
@@ -42,60 +76,63 @@ function CarWriter (Block) {
       return new Promise((resolve) => {
         resolver = () => {
           if (queue.length) {
-            resolve({ done: false, value: queue.shift() })
+            if (queue.length === 1) {
+              if (drainer) {
+                drainerResolver()
+                drainerResolver = null
+                drainer = null
+              }
+            }
+            return resolve({ done: false, value: queue.shift() })
           }
 
           if (ended) {
             return resolve({ done: true })
+          /* c8 ignore next 5 */
           } else {
-            return resolve(iterator.next()) // recurse until we find something?
+            // recurse until we find something; but this might not be a
+            // possible path with the mutex in place
+            return resolve(iterator.next())
           }
         }
       })
     }
   }
 
-  // TODO: cb should be async here, should have back-pressure
   const encoder = Encoder(Block, (chunk) => {
     queue.push(chunk)
+    if (!drainer) {
+      drainer = new Promise((resolve) => {
+        drainerResolver = resolve
+      })
+    }
     if (resolver) {
       resolver()
       resolver = null
     }
+    return drainer
   })
 
   let mutex = Promise.resolve()
 
   class CarWriter {
     constructor (roots) {
-      if (roots && roots.asCID === roots) {
-        roots = [roots]
-      }
-      if (roots === undefined) {
-        roots = []
-      }
-      if (!Array.isArray(roots)) {
-        throw new TypeError('roots must be a single CID or an array of CIDs')
-      }
-      const _roots = []
-      for (const root of roots) {
-        const _root = CID.asCID(root)
-        if (!_root) {
-          throw new TypeError('roots must be a single CID or an array of CIDs')
-        }
-        _roots.push(_root)
-      }
-      mutex = encoder.setRoots(_roots)
+      mutex = encoder.setRoots(toRoots(roots))
     }
 
     async put (block) {
+      if (typeof block.encode !== 'function' || typeof block.cid !== 'function') {
+        throw new TypeError('Can only write Block objects')
+      }
       mutex = mutex.then(() => encoder.writeBlock(block))
       return mutex
     }
 
     async close () {
+      if (ended) {
+        throw new Error('Already closed')
+      }
       await mutex
-      // TODO: can only call this once
       ended = true
       if (resolver) {
         resolver()
@@ -104,7 +141,10 @@ function CarWriter (Block) {
     }
 
     [Symbol.asyncIterator] () {
-      // TODO: single call allowed to this method
+      if (iterating) {
+        throw new Error('Multiple iterator not supported')
+      }
+      iterating = true
       return iterator
     }
   }
--- test/test-writer.js
+++ test/test-writer.js
@@ -3,6 +3,8 @@
 import { CarWriter } from 'datastore-car'
 import { Block, carBytes, makeData, assert } from './common.js'
 
+const { toHex } = Block.multiformats.bytes
+
 function collector (iterable) {
   return (async () => {
     const chunks = []
@@ -28,8 +30,8 @@ describe('CarWriter', () => {
 
   function assertCarData (actual) {
     assert.strictEqual(
-      Block.multiformats.bytes.toHex(actual),
-      Block.multiformats.bytes.toHex(carBytes),
+      toHex(actual),
+      toHex(carBytes),
       'got expected bytes'
     )
   }
@@ -108,12 +110,80 @@ describe('CarWriter', () => {
     const writer = CarWriter(Block).create(roots)
 
     for (const block of allBlocksFlattened) {
-      await writer.put(block)
+      writer.put(block)
     }
-    await writer.close()
+    writer.close()
 
     const collection = collector(writer)
     const bytes = await collection
     assertCarData(bytes)
+  })
+
+  it('single root', async () => {
+    const writer = CarWriter(Block).create(roots[0])
+    const collection = collector(writer)
+
+    for (const block of allBlocksFlattened) {
+      await writer.put(block)
+    }
+    await writer.close()
+
+    const bytes = await collection
+
+    // test the start of the bytes to make sure we have the root def block we expect
+    // { roots: [ CID(bafyreihyrpefhacm6kkp4ql6j6udakdit7g3dmkzfriqfykhjw6cad5lrm) ], version: 1 }
+    const expectedRootDef = 'a265726f6f747381d82a58250001711220f88bc853804cf294fe417e4fa83028689fcdb1b1592c5102e1474dbc200fab8b6776657273696f6e01'
+    const expectedStart = (expectedRootDef.length / 2).toString(16) + // length of root def block
+      expectedRootDef +
+      '28' // length of first raw block + CIDv0
+
+    assert.strictEqual(toHex(bytes).substring(0, expectedStart.length), expectedStart)
+  })
+
+  it('no roots', async () => {
+    const writer = CarWriter(Block).create()
+    const collection = collector(writer)
+
+    for (const block of allBlocksFlattened) {
+      await writer.put(block)
+    }
+    await writer.close()
+
+    const bytes = await collection
+
+    // test the start of the bytes to make sure we have the root def block we expect
+    // { roots: [], version: 1 }
+    const expectedRootDef = 'a265726f6f7473806776657273696f6e01'
+    const expectedStart = (expectedRootDef.length / 2).toString(16) + // length of root def block
+      expectedRootDef +
+      '28' // length of first raw block + CIDv0
+
+    assert.strictEqual(toHex(bytes).substring(0, expectedStart.length), expectedStart)
+  })
+
+  it('bad argument for create()', () => {
+    for (const arg of [new Uint8Array(0), true, false, null, 'string', 100, { obj: 'nope' }, [false]]) {
+      assert.throws(() => CarWriter(Block).create(arg))
+    }
+  })
+
+  it('bad argument for put()', async () => {
+    const writer = CarWriter(Block).create()
+    for (const arg of [new Uint8Array(0), true, false, null, 'string', 100, { obj: 'nope' }, [false]]) {
+      await assert.isRejected(writer.put(arg))
+    }
+  })
+
+  it('bad attempt to multiple iterate', async () => {
+    const writer = CarWriter(Block).create()
+    collector(writer)
+    await assert.isRejected(collector(writer), /multiple iterator/i)
+  })
+
+  it('bad attempt to multiple close', async () => {
+    const writer = CarWriter(Block).create()
+    collector(writer)
+    await writer.close()
+    await assert.isRejected(writer.close(), /closed/i)
   })
 })
