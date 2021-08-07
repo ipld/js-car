d4c8e966d42bb3c3390b3dae4ab26e3046a75449
Rod Vagg <rod@vagg.org>
2021-08-07 14:42:47 +1000

    fix: handle zero-byte chunks on decode, don't write zero-byte chunks on encode
    Fixes: https://github.com/ipld/js-car/issues/46
    Fixes: https://github.com/web3-storage/web3.storage/issues/302
    Ref: https://github.com/web3-storage/ipfs-car/pull/73

--- test/test-reader.js
+++ test/test-reader.js
@@ -1,6 +1,10 @@
 /* eslint-env mocha */
 
 import { CarReader } from '@ipld/car/reader'
+import { CarWriter } from '@ipld/car/writer'
+import * as Block from 'multiformats/block'
+import { sha256 } from 'multiformats/hashes/sha2'
+import * as raw from 'multiformats/codecs/raw'
 import { carBytes, makeIterable, assert } from './common.js'
 import {
   verifyRoots,
@@ -80,6 +84,31 @@ describe('CarReader fromIterable()', () => {
     await verifyGet(reader)
     await verifyBlocks(reader.blocks())
     await verifyCids(reader.cids())
+  })
+
+  it('handle zero-byte chunks', async () => {
+    // write 3 blocks, the middle one has zero bytes - this is a valid dag-pb form
+    // so it's important that we can handle it .. also we may just be dealing with
+    // an asynciterator that provides zero-length chunks
+    const { writer, out } = await CarWriter.create([])
+    const b1 = await Block.encode({ value: Uint8Array.from([0, 1, 2]), hasher: sha256, codec: raw })
+    writer.put(b1)
+    const b2 = await Block.encode({ value: Uint8Array.from([]), hasher: sha256, codec: raw })
+    writer.put(b2)
+    const b3 = await Block.encode({ value: Uint8Array.from([3, 4, 5]), hasher: sha256, codec: raw })
+    writer.put(b3)
+    const closePromise = writer.close()
+    const reader = await CarReader.fromIterable(out) // read from the writer
+    const b1a = await reader.get(b1.cid)
+    assert.isDefined(b1a)
+    assert.deepStrictEqual(b1a && Array.from(b1a.bytes), [0, 1, 2])
+    const b2a = await reader.get(b2.cid)
+    assert.isDefined(b2a)
+    assert.deepStrictEqual(b2a && Array.from(b2a.bytes), [])
+    const b3a = await reader.get(b3.cid)
+    assert.isDefined(b3a)
+    assert.deepStrictEqual(b3a && Array.from(b3a.bytes), [3, 4, 5])
+    await closePromise
   })
 
   it('bad argument', async () => {
